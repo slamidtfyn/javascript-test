421e1795c898993dbb84e3fde8d223380c1adccb
Søren Larsen <slamidtfyn@gmail.com>
2020-05-14 13:26:54 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,1 +1,4 @@
-console.log('widget')
+console.log('test');
+let x= document.createElement('div');
+x.style="background-color: red; position: fixed;width:100px;height:100px;bottom: 10px;left: 10px;";
+document.body.appendChild(x);

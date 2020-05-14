6613a3a1435913e99c1d6d3d0addd5fda9d9c14e
Søren Larsen <slamidtfyn@gmail.com>
2020-05-14 13:32:52 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -3,4 +3,4 @@ let x= document.createElement('div');
 x.style="background-color: red; position: fixed;width:100px;height:100px;bottom: 10px;left: 10px;";
 x.addEventListener('click',CookieConsent.show);
 document.body.appendChild(x);
-f
+
